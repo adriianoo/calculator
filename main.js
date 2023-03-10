87786ba84fd779655a03271ceb7fc88178b5fa98
adriianoo <adrijimenezh@hotmail.com>
2023-03-10 14:21:28 +0100

    added basic math operators

---
+++ main.js
@@ -0,0 +1,21 @@
+// Functions for basic math operators
+
+
+const add = (a, b) => {
+    return a + b;
+}
+
+
+const subtract = (a, b) => {
+    return a - b;
+}
+
+
+const multiply = (a, b) => {
+    return a * b;
+}
+
+
+const divide = (a, b) => {
+    return a / b;
+}
