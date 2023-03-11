e93c341a1cc04aa6cac5d5102956df39d5b86edc
adriianoo <adrijimenezh@hotmail.com>
2023-03-11 11:32:18 +0100

    added new operate function

--- main.js
+++ main.js
@@ -19,3 +19,24 @@ const multiply = (a, b) => {
 const divide = (a, b) => {
     return a / b;
 }
+
+
+const operate = (operator, a, b) => {
+    
+    if(operator === '+') 
+    {
+        return add(a, b);
+    }
+    else if(operator === '-') 
+    {
+        return subtract(a, b);
+    }
+    else if(operator === '-') 
+    {
+        return multiply(a, b);
+    }
+    else if(operator === '-') 
+    {
+        return subtract(a, b);
+    }
+}
